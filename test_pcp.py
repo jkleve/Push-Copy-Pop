12de3b073232a6bbaa18b06ec9fc81cb97f1d954
Jesse Kleve <jesse.kleve@gmail.com>
2017-07-09 12:58:08 -0600

    Added more UTs. One failing unit test that needs to be fixed when adding two files to stack

--- test_pcp.py
+++ test_pcp.py
@@ -3,18 +3,39 @@ import pcp
 import sys
 import unittest
 
+
 class TestPush(unittest.TestCase):
     def setUp(self):
-        pass
+        print("\n================")
+        print("Testing {}".format(self.id()))
+        pcp.empty_stack()
+        sys.argv.append("push")
 
     def tearDown(self):
         pcp.empty_stack()
+        del sys.argv[1:]
+        print("Done testing {}".format(self.id()))
+        print("================")
 
     def test_file_does_not_exist(self):
-        sys.argv.append("push")
-        sys.argv.append("LICENSE")
+        sys.argv.append("LICENSES")
+        pcp._ArgsHandler.push()
+        self.assertEqual(len(pcp._stack), 0)
+
+    def test_file_with_period(self):
+        sys.argv.append("./LICENSE")
         pcp._ArgsHandler.push()
         self.assertEqual(len(pcp._stack), 1)
+
+    def test_file_with_double_period(self):
+        sys.argv.append("../Push-Copy-Pop/LICENSE")
+        pcp._ArgsHandler.push()
+        self.assertEqual(len(pcp._stack), 1)
+
+    def test_two_files(self):
+        sys.argv.append("LICENSE README.md")
+        pcp._ArgsHandler.push()
+        self.assertEqual(len(pcp._stack), 2)
 
 
 if __name__ == "__main__":
